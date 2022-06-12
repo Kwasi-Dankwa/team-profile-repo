2d3f0bcd2e03e801481b4237147b663d9623e11e
Kwasi Dankwa <edankwa14@gmail.com>
2022-06-11 22:13:10 -0400

    Pushing my first files

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//generate html page to create employee cards and loop through array
+function generateHTML() {
+    const htmlpage = [];
+    const htmlpagehead = ``
+}
+
